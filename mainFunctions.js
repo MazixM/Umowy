e2adb2ac5c5a7f7a8ca194ca0e656a4cfd3e450a
Marcin Milczarek <50635945+MazixM@users.noreply.github.com>
2020-12-27 14:14:07 +0100

    Add new format

--- mainFunctions.js
+++ mainFunctions.js
@@ -1,144 +1,178 @@
 var today = new Date();
 
 var storage = (function () {
+  var setData = function (key, obj) {
+    var values = JSON.stringify(obj);
+    localStorage.setItem(key, values);
+  };
 
-    var setData = function (key, obj) {
-        var values = JSON.stringify(obj);
-        localStorage.setItem(key, values);
+  var getData = function (key) {
+    if (localStorage.getItem(key) != null) {
+      return JSON.parse(localStorage.getItem(key));
+    } else {
+      return false;
     }
+  };
 
-    var getData = function (key) {
-        if (localStorage.getItem(key) != null) {
-            return JSON.parse(localStorage.getItem(key));
-        } else {
-            return false;
-        }
+  var updateDate = function (key, newData) {
+    if (localStorage.getItem(key) != null) {
+      var oldData = JSON.parse(localStorage.getItem(key));
+      for (keyObj in newData) {
+        oldData[keyObj] = newData[keyObj];
+      }
+      var values = JSON.stringify(oldData);
+      localStorage.setItem(key, values);
+    } else {
+      return false;
     }
+  };
 
-    var updateDate = function (key, newData) {
-        if (localStorage.getItem(key) != null) {
-            var oldData = JSON.parse(localStorage.getItem(key));
-            for (keyObj in newData) {
-                oldData[keyObj] = newData[keyObj];
-            }
-            var values = JSON.stringify(oldData);
-            localStorage.setItem(key, values);
-        } else {
-            return false;
-        }
+  var logData = function (text) {
+    var time = Date.now();
+    var log = JSON.parse(localStorage.getItem("log"));
+    if (log) {
+      log[Object.keys(log).length + 1] = {
+        time: ConvertTimeToString(time),
+        log: text,
+      };
+      localStorage.setItem("log", JSON.stringify(log));
+    } else {
+      localStorage.setItem(
+        "log",
+        JSON.stringify({ time: ConvertTimeToString(time), log: text })
+      );
     }
+  };
 
-    var logData = function (text) {
-        var time = Date.now();
-        var log = JSON.parse(localStorage.getItem("log"));
-        if (log) {
-            log[Object.keys(log).length + 1] = { "time": ConvertTimeToString(time), "log": text }
-            localStorage.setItem("log", JSON.stringify(log));
-        } else {
-            localStorage.setItem("log", JSON.stringify({ "time": ConvertTimeToString(time), "log": text }));
-        }
-    }
-
-    return { set: setData, get: getData, update: updateDate, log: logData }
+  return { set: setData, get: getData, update: updateDate, log: logData };
 })();
 
 function isValidDate(d) {
-    return d instanceof Date && !isNaN(d);
+  return d instanceof Date && !isNaN(d);
 }
 
 function ConvertTimeToString(time) {
-    var newData = new Date(time);
-    var dd = String(newData.getDate()).padStart(2, '0');
-    var mm = String(newData.getMonth() + 1).padStart(2, '0');
-    var yyyy = newData.getFullYear();
+  var newData = new Date(time);
+  var dd = String(newData.getDate()).padStart(2, "0");
+  var mm = String(newData.getMonth() + 1).padStart(2, "0");
+  var yyyy = newData.getFullYear();
 
-    return dd + '/' + mm + '/' + yyyy;
+  return dd + "/" + mm + "/" + yyyy;
 }
 
 function isJsonStringCorrect(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 function createTableFromLocalvalues(divId) {
-    var firstRow = "<tr><td colspan=\"5\"><b>Aktualne umowy</b></td></tr>" +
-        "<tr><td><b>Nazwa</b></td><td><b>Data od</b></td><td><b>Data do</b></td><td><b>h/miesiąc</b></td><td><b>h/rok</b></td></tr>";
-    var expiredRow = "<tr><td colspan=\"5\"><b>Nieaktualne umowy</b></td></tr>";
-    var actualContractRow = "";
-    var expiredContractRow = "";
-    var records = storage.get("records");
+  var firstRow =
+    '<tr><td colspan="5"><b>Aktualne umowy</b></td></tr>' +
+    "<tr><td><b>Nazwa</b></td><td><b>Data od</b></td><td><b>Data do</b></td><td><b>h/miesiąc</b></td><td><b>h/rok</b></td></tr>";
+  var expiredRow = '<tr><td colspan="5"><b>Nieaktualne umowy</b></td></tr>';
+  var actualContractRow = "";
+  var expiredContractRow = "";
+  var records = storage.get("records");
 
-    var color = "";
-    jQuery.each(records, function (_i, value) {
-        if (value.dateTo <= today.getTime()) {
-            color = "red";
-        } else if ((value.dateTo - today.getTime()) > 1209600000)// 14 days // 604800000) // 7 days
-        {
-            color = "green";
-        } else {
-            color = "yellow";
-        }
-        var currentRow = "<tr style='background-color: " + color + "'><td>" + value.name + "</td><td>" + ConvertTimeToString(value.dateFrom) + "</td><td>" + ConvertTimeToString(value.dateTo) + "</td><td><a href='" + value.urlHoursMonth + "' target='_blank'>" + value.hoursMonth + " / " + value.hoursMonthMax + "<a/></td><td><a href='" + value.urlHoursYear + "' target='_blank'>" + value.hoursYear + " / " + value.hoursYearMax + "</a></td></tr>";
-        if (color == "red") {
-            expiredContractRow += currentRow;
-        } else {
-            actualContractRow += currentRow;
-        }
-    });
+  var color = "";
+  jQuery.each(records, function (_i, value) {
+    if (value.dateTo <= today.getTime()) {
+      color = "red";
+    } else if (value.dateTo - today.getTime() > 1209600000) {
+      // 14 days // 604800000) // 7 days
+      color = "green";
+    } else {
+      color = "yellow";
+    }
+    var currentRow =
+      "<tr style='background-color: " +
+      color +
+      "'><td>" +
+      value.name +
+      "</td><td>" +
+      ConvertTimeToString(value.dateFrom) +
+      "</td><td>" +
+      ConvertTimeToString(value.dateTo) +
+      "</td><td><a href='" +
+      value.urlHoursMonth +
+      "' target='_blank'>" +
+      value.hoursMonth +
+      " / " +
+      value.hoursMonthMax +
+      "<a/></td><td><a href='" +
+      value.urlHoursYear +
+      "' target='_blank'>" +
+      value.hoursYear +
+      " / " +
+      value.hoursYearMax +
+      "</a></td></tr>";
+    if (color == "red") {
+      expiredContractRow += currentRow;
+    } else {
+      actualContractRow += currentRow;
+    }
+  });
 
-    document.getElementById(divId).innerHTML = (firstRow + actualContractRow + expiredRow + expiredContractRow);
+  document.getElementById(divId).innerHTML =
+    firstRow + actualContractRow + expiredRow + expiredContractRow;
 }
 
 function copyTextToClipboardById(id) {
-    var copyText = document.getElementById(id);
-    copyText.select();
-    document.execCommand("copy");
+  var copyText = document.getElementById(id);
+  copyText.select();
+  document.execCommand("copy");
 
-    return copyText.value;
+  return copyText.value;
 }
 
 function getTimeByUrl(webURL) {
-    const regexFilter = /<span class="value">(?<time>[0-9]{0,5}:[0-9]{0,2})<\/span>/;
-    const htmlContent = $.ajax({
-        url: webURL,
-        async: false
-    }).responseText;
-    if (htmlContent) {
-        var regex = htmlContent.match(regexFilter);
-        if (regex) {
-            return regex[0];
-        }
+  const regexFilter = /<span class="value">(?<time>[0-9]{0,5}:[0-9]{0,2})<\/span>/;
+  const htmlContent = $.ajax({
+    url: webURL,
+    async: false,
+  }).responseText;
+  if (htmlContent) {
+    var regex = htmlContent.match(regexFilter);
+    if (regex) {
+      return regex[0];
     }
-    storage.log("Nie znaleziono godziny dla URL: " + webURL)
+  }
+  storage.log("Nie znaleziono godziny dla URL: " + webURL);
 
-    return "";
+  return "";
 }
 
 function updateLocalRecordsTime() {
-    var records = storage.get("records");
-    var progress = document.getElementById("progressBar");
-    progress.value = 0;
-    progress.max = Object.keys(records).length;
+  var records = storage.get("records");
+  var progress = document.getElementById("progressBar");
+  progress.value = 0;
+  progress.max = Object.keys(records).length;
 
-    jQuery.each(records, function (_i, value) {
-        value.hoursMonth = getTimeByUrlOrReturnDefault(value.urlHoursMonth, value.hoursMonth);
-        value.hoursYear = getTimeByUrlOrReturnDefault(value.urlHoursYear, value.hoursYear);
-    });
-    storage.set("records", records);
+  jQuery.each(records, function (_i, value) {
+    value.hoursMonth = getTimeByUrlOrReturnDefault(
+      value.urlHoursMonth,
+      value.hoursMonth
+    );
+    value.hoursYear = getTimeByUrlOrReturnDefault(
+      value.urlHoursYear,
+      value.hoursYear
+    );
+  });
+  storage.set("records", records);
 }
 
 function getTimeByUrlOrReturnDefault(webUrl, defaultValue) {
-    if (webUrl) {
-        var time = getTimeByUrl(webUrl);
-        if (time) {
-            return time;
-        }
+  if (webUrl) {
+    var time = getTimeByUrl(webUrl);
+    if (time) {
+      return time;
     }
+  }
 
-    return defaultValue;
-}   
+  return defaultValue;
+}
--- options.js
+++ options.js
@@ -1,155 +1,164 @@
 var today = new Date();
-document.addEventListener('DOMContentLoaded', function () {
-    //OnLoad do :
-    document.getElementById('addNew').addEventListener('click', function () {
-        addNewContract();
-        refreshTable();
-    });
-    document.getElementById('deleteAllButton').addEventListener('click', function () {
-        if (confirm("Czy na pewno chcesz usunąć wszystkie rekordy?")) {
-            deleteAllContracts();
-        }
-    });
-    document.getElementById('deleteContractButton').addEventListener('click', function () {
-        var name = prompt("Podaj nazwę, którą chcesz usunąć", "");
-        if (name) {
-            deleteContractByName(name);
-            refreshTable();
-        }
-    });
-    document.getElementById('exportButton').addEventListener('click', function () {
-        exportContracts();
-    });
-    document.getElementById('importButton').addEventListener('click', function () {
-        if (confirm("Obecne rekordy zostaną usunięte, czy chcesz kontynuować?")) {
-            importContracts();
-        }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  //OnLoad do :
+  document.getElementById("addNew").addEventListener("click", function () {
+    addNewContract();
     refreshTable();
+  });
+  document
+    .getElementById("deleteAllButton")
+    .addEventListener("click", function () {
+      if (confirm("Czy na pewno chcesz usunąć wszystkie rekordy?")) {
+        deleteAllContracts();
+      }
+    });
+  document
+    .getElementById("deleteContractButton")
+    .addEventListener("click", function () {
+      var name = prompt("Podaj nazwę, którą chcesz usunąć", "");
+      if (name) {
+        deleteContractByName(name);
+        refreshTable();
+      }
+    });
+  document
+    .getElementById("exportButton")
+    .addEventListener("click", function () {
+      exportContracts();
+    });
+  document
+    .getElementById("importButton")
+    .addEventListener("click", function () {
+      if (confirm("Obecne rekordy zostaną usunięte, czy chcesz kontynuować?")) {
+        importContracts();
+      }
+    });
+  refreshTable();
 });
 
 function refreshTable() {
-    if (storage.get("records")) {
-        createTableFromLocalvalues("mainTable");
-    } else {
-        document.getElementById("mainTable").innerHTML = "";
-    }
+  if (storage.get("records")) {
+    createTableFromLocalvalues("mainTable");
+  } else {
+    document.getElementById("mainTable").innerHTML = "";
+  }
 }
 
 function exportContracts() {
-    document.getElementById('importExport').value = JSON.stringify(storage.get("records"));
-    copyTextToClipboardById('importExport');
-    alert("Pomyślnie skopiowano do schowka.");
+  document.getElementById("importExport").value = JSON.stringify(
+    storage.get("records")
+  );
+  copyTextToClipboardById("importExport");
+  alert("Pomyślnie skopiowano do schowka.");
 }
 
 function importContracts() {
-    var textToImport = document.getElementById('importExport').value;
-    if (isJsonStringCorrect(textToImport)) {
-        var records = JSON.parse(textToImport);
-        storage.set("records", records);
-        refreshTable();
-        alert("Pomyślnie zaimportowano rekordy.");
-    } else {
-        alert("Podany JSON jest błędny.");
-    }
+  var textToImport = document.getElementById("importExport").value;
+  if (isJsonStringCorrect(textToImport)) {
+    var records = JSON.parse(textToImport);
+    storage.set("records", records);
+    refreshTable();
+    alert("Pomyślnie zaimportowano rekordy.");
+  } else {
+    alert("Podany JSON jest błędny.");
+  }
 }
 
 function addNewContract() {
-    var name = document.getElementById("name").value;
-    var dateFrom = document.getElementById("dateFrom").value;
-    var dateTo = document.getElementById("dateTo").value;
-    var urlHoursMonth = document.getElementById("urlHoursMonth").value;
-    var urlHoursYear = document.getElementById("urlHoursYear").value;
-    var hoursMonthMax = document.getElementById("hoursMonthMax").value;
-    var hoursYearMax = document.getElementById("hoursYearMax").value;
+  var name = document.getElementById("name").value;
+  var dateFrom = document.getElementById("dateFrom").value;
+  var dateTo = document.getElementById("dateTo").value;
+  var urlHoursMonth = document.getElementById("urlHoursMonth").value;
+  var urlHoursYear = document.getElementById("urlHoursYear").value;
+  var hoursMonthMax = document.getElementById("hoursMonthMax").value;
+  var hoursYearMax = document.getElementById("hoursYearMax").value;
 
-    dateFrom = new Date(dateFrom);
-    dateTo = new Date(dateTo);
-    
-    if (name.length < 3 || name.length > 20) {
-        alert("Podaj nazwę [3-20]");
+  dateFrom = new Date(dateFrom);
+  dateTo = new Date(dateTo);
 
-        return;
+  if (name.length < 3 || name.length > 20) {
+    alert("Podaj nazwę [3-20]");
+
+    return;
+  }
+  if (!(isValidDate(dateFrom) && isValidDate(dateTo))) {
+    alert("Wybierz datę");
+
+    return;
+  }
+  if (hoursMonthMax.length == 0) {
+    alert("Podaj liczbę godzin/Miesiąc");
+
+    return;
+  }
+  if (hoursYearMax.length == 0) {
+    alert("Podaj liczbę godzin/Rok");
+
+    return;
+  }
+  if (urlHoursMonth) {
+    if (!getTimeByUrl(urlHoursMonth)) {
+      alert("Podany link h/Miesiąc jest błędny lub nie jesteś zalogowany");
+
+      return;
     }
-    if (!(isValidDate(dateFrom) && isValidDate(dateTo))) {
-        alert("Wybierz datę");
+  } else {
+    alert("Podany link h/Miesiąc");
 
-        return;
+    return;
+  }
+  if (urlHoursYear) {
+    if (!getTimeByUrl(urlHoursYear)) {
+      alert("Podany link h/Rok jest błędny lub nie jesteś zalogowany");
+
+      return;
     }
-    if (hoursMonthMax.length == 0) {
-        alert("Podaj liczbę godzin/Miesiąc");
+  } else {
+    alert("Podany link h/Rok");
 
-        return;
-    }
-    if (hoursYearMax.length == 0) {
-        alert("Podaj liczbę godzin/Rok");
+    return;
+  }
 
-        return;
-    }
-    if (urlHoursMonth) {
-        if (!getTimeByUrl(urlHoursMonth)) {
-            alert("Podany link h/Miesiąc jest błędny lub nie jesteś zalogowany");
-
-            return;
-        }
-    } else {
-        alert("Podany link h/Miesiąc");
-
-        return;
-    }
-    if (urlHoursYear) {
-        if (!getTimeByUrl(urlHoursYear)) {
-            alert("Podany link h/Rok jest błędny lub nie jesteś zalogowany");
-
-            return;
-        }
-    } else {
-        alert("Podany link h/Rok");
-
-        return;
-    }
-
-
-    var newContract = {
-        "name": name,
-        "dateFrom": dateFrom.getTime(),
-        "dateTo": dateTo.getTime(),
-        "urlHoursMonth": urlHoursMonth,
-        "urlHoursYear": urlHoursYear,
-        "hoursMonth": 0,
-        "hoursMonthMax": hoursMonthMax,
-        "hoursYear": 0,
-        "hoursYearMax": hoursYearMax
-    };
-    var records = storage.get("records");
-    if (!records) {
-        records = {};
-    }
-    records[Object.keys(records).length + 1] = newContract;
-    storage.set("records", records);
-    alert("Dodano");
+  var newContract = {
+    name: name,
+    dateFrom: dateFrom.getTime(),
+    dateTo: dateTo.getTime(),
+    urlHoursMonth: urlHoursMonth,
+    urlHoursYear: urlHoursYear,
+    hoursMonth: 0,
+    hoursMonthMax: hoursMonthMax,
+    hoursYear: 0,
+    hoursYearMax: hoursYearMax,
+  };
+  var records = storage.get("records");
+  if (!records) {
+    records = {};
+  }
+  records[Object.keys(records).length + 1] = newContract;
+  storage.set("records", records);
+  alert("Dodano");
 }
 
 function deleteContractByName(name) {
-    var records = storage.get("records");
-    var removed = false;
-    jQuery.each(records, function (_i, value) {
-        if (value.name == name) {
-            delete records[_i];
-            removed = true;
-            return false;
-        }
-    });
-    if (removed) {
-        storage.set("records", records);
-        alert("Pomyślnie usnięto '" + name + "'");
-    } else {
-        alert("Nie znaleziono rekordu o nazwie '" + name + "'");
+  var records = storage.get("records");
+  var removed = false;
+  jQuery.each(records, function (_i, value) {
+    if (value.name == name) {
+      delete records[_i];
+      removed = true;
+      return false;
     }
+  });
+  if (removed) {
+    storage.set("records", records);
+    alert("Pomyślnie usnięto '" + name + "'");
+  } else {
+    alert("Nie znaleziono rekordu o nazwie '" + name + "'");
+  }
 }
 
 function deleteAllContracts() {
-    storage.set("records", false);
-    refreshTable();
-    alert("Pomyślnie usunięto wszystkie rekordy.");
+  storage.set("records", false);
+  refreshTable();
+  alert("Pomyślnie usunięto wszystkie rekordy.");
 }
